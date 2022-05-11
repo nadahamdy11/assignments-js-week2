087e4bd317fbd15cdaac517667cad02d7fe3fa66
unknown <nh998449gmail.com>
2022-05-11 04:52:13 +0200

    first commit

---
+++ assignment2.js
@@ -0,0 +1,13 @@
+//assinment 2 in week2 in javascript 
+let a = 21;
+let b = 20;
+underscore = " _ ";
+result = underscore + b + String(a);
+//do not repeat the variable twice
+//we have _21_2021_2021_2021_20_
+console.log(underscore + a + result.repeat(3) + underscore + b + underscore);
+ 
+//assignment 3 in week2 in javascript
+ 
+console.log(10 * 20 % (15 * 3) - (190 + 10) % 400);//equal => 0
+ 
